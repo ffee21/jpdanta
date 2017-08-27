8be208a5ab9f25199ba74a7f32920e09d6c700d8
ffee21 <ffee21@gmail.com>
2017-08-27 21:58:33 +0900

    node.js web server

--- sandbox/sandbox4.js
+++ sandbox/sandbox4.js
@@ -1,12 +1,13 @@
 var PythonShell = require('python-shell');
 var CronJob = require('cron').CronJob;
 
-new CronJob('0 * * * * *', function () {
-    console.log(new Date().toString())
+new CronJob('*/15 * * * * *', function () {
+    var starttime = new Date().toString();
+    console.log(starttime + "/" + new Date().toString() + " : start")
     
     PythonShell.run('sandbox/sandbox3.py', function (err) {
         if (err) throw err;
-        console.log('finished');
+        console.log(starttime + "/" + new Date().toString() + ' : finish');
       });
 }, null, true);
 
